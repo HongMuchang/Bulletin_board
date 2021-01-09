a941ec4f845de4edfd1c73feebf58de02e288db7
HongMuchan <muchang0824@gmail.com>
2021-01-09 22:21:03 +0900

    Next_Start

---
+++ pages/index.tsx
@@ -0,0 +1,14 @@
+import { NextPage } from 'next'
+import 'tailwindcss/tailwind.css'
+// import Head from 'next/head'
+// import styles from '../styles/Home.module.scss'
+
+const Home: NextPage = () => {
+  return (
+    <div>
+      <p>HelloWorld</p>
+    </div>
+  )
+}
+
+export default Home
