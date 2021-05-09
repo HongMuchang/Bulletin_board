b3fa1d06c51f3a8c7bf6ba40ac19b642fc860974
HongMuchan <muchang0824@gmail.com>
2021-05-09 23:29:33 +0900

    first commit

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,6 +7,9 @@ const Home: NextPage = () => {
   return (
     <div>
       <p>HelloWorld</p>
+      <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+        Button
+      </button>
     </div>
   )
 }
