6c1c03099b1637bb156ec67716a96033bbb843f8
HongMuchan <muchang0824@gmail.com>
2021-05-10 00:25:40 +0900

    delete #1 components

--- pages/_app.tsx
+++
@@ -1,9 +0,0 @@
-import { FC } from 'react'
-import { AppProps } from 'next/app'
-import '../styles/globals.scss'
-
-const MyApp: FC<AppProps> = ({ Component, pageProps }: AppProps) => {
-  return <Component {...pageProps} />
-}
-
-export default MyApp
