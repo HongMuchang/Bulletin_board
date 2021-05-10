0acd40f57848740217764c987a842db96f8b9d48
HongMuchan <muchang0824@gmail.com>
2021-05-10 23:16:18 +0900

    layoutComponents

--- src/components/atoms/layout/Footer.tsx
+++ src/components/atoms/layout/Footer.tsx
@@ -3,8 +3,8 @@ import type { VFC } from 'react'
 export const Footer: VFC = () => {
   return (
     <>
-      <footer className="w-full h-12 flex justify-center items-center border-t">
-        footer
+      <footer className="bg-gray-800  w-full h-12 flex justify-center text-white items-center border-t">
+        muchan
       </footer>
     </>
   )
--- src/components/atoms/layout/Header.tsx
+++ src/components/atoms/layout/Header.tsx
@@ -8,7 +8,7 @@ export const Header: VFC = () => {
         <div className="flex items-center pl-8 h-14">
           <div className="flex space-x-4">
             <Link href="/">
-              <a className="text-gray-300 hover:bg-gray-700 px-3 py-2 rounded">
+              <a className="text-gray-300 hover:bg-gray-700  px-3 py-2 rounded">
                 Home
               </a>
             </Link>
