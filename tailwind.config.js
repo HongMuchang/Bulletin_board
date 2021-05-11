fa279acd9c84512ef7f80bf21d49d3fca86025e4
HongMuchan <muchang0824@gmail.com>
2021-05-11 20:46:13 +0900

    settings

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  purge: ['./pages/**/*.js', './components/**/*.js'],
+  purge: ['./src/**/*.{js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
