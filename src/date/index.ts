e19ac635f889d038b8bca0ff1b822792e9dce01a
HongMuchan <muchang0824@gmail.com>
2021-05-11 20:45:58 +0900

    Date

---
+++ src/date/index.ts
@@ -0,0 +1,44 @@
+export const Date = [
+  {
+    id: 1,
+    title: 'Recoil',
+    link: 'https://google.com',
+    category: 'React',
+  },
+  {
+    id: 2,
+    title: 'Recoil',
+    link: 'https://hongmuchan.net',
+    category: 'Tailwindcss',
+  },
+  {
+    id: 3,
+    title: 'Recoil',
+    link: 'https://google.com',
+    category: 'React',
+  },
+  {
+    id: 4,
+    title: 'Recoil',
+    link: 'https://google.com',
+    category: 'React',
+  },
+  {
+    id: 5,
+    title: 'Recoil',
+    link: 'https://google.com',
+    category: 'React',
+  },
+  {
+    id: 6,
+    title: 'Recoil',
+    link: 'https://google.com',
+    category: 'React',
+  },
+  {
+    id: 7,
+    title: 'Recoil',
+    link: 'https://google.com',
+    category: 'React',
+  },
+]
